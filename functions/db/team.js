5650db80a07131ee1c69e3f2b7ec7a25af37e0f8
Youjung Heo <hujumee27@gmail.com>
2022-01-14 03:32:23 +0900

    chore:> merge brach dev

--- functions/db/team.js
+++ functions/db/team.js
@@ -1,6 +1,18 @@
 const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
+const addTeam = async (client, name, image, description) => {
+    const { rows } = await client.query(
+        `
+        INSERT INTO team
+        ("name", image, description)
+        VALUES 
+        ($1, $2, $3)
+        RETURNING *
+        `,
+        [name, image, description]
+    );
+    return convertSnakeToCamel.keysToCamel(rows[0]);
+};
 
-
-module.exports = { };
+module.exports = { addTeam };
