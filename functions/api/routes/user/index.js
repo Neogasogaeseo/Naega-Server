c1ec65571a4b42570af6bb48839a47b17e6f0366
realwhyjay <brandon94_@naver.com>
2022-01-14 01:54:14 +0900

    fix:> test 파일 정리, firebase.json 재생성

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', require('./testGET'));
 
 module.exports = router;
--- functions/db/index.js
+++ functions/db/index.js
@@ -1,4 +1,3 @@
 module.exports = {
-  testDB: require('./test'),
   userDB: require('./user'),
 };
