9bfb1b241a0425e96e27ed5170668b0e53ab9984
Youjung Heo <hujumee27@gmail.com>
2022-01-14 21:51:26 +0900

    chore:> conflict 해결

--- functions/api/routes/user/keywordCreatePOST.js
+++ functions/api/routes/user/keywordCreatePOST.js
@@ -3,22 +3,12 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
-<<<<<<< HEAD:functions/api/routes/team/teamPOST.js
-const { teamDB } = require('../../../db')
-
-module.exports = async (req, res) => {
-
-  const { name, image, description } = req.body
-  
-  if (!name) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-=======
 
 module.exports = async (req, res) => {
 
   const {} = req.body
   
 //   if (!) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
->>>>>>> a03c670f1b0dc5ec4dc35ef29023eec9a3a68e65:functions/api/routes/user/keywordCreatePOST.js
   
   let client;
   
@@ -27,15 +17,9 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-<<<<<<< HEAD:functions/api/routes/team/teamPOST.js
-    const teamData = await teamDB.addTeam(client, name, image, description);
-    
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.POST_TEAM, teamData));
-=======
     // const DB데이터 = await 파일이름DB.쿼리문이름(client);
     
     // res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_USERS_SUCCESS, DB데이터));
->>>>>>> a03c670f1b0dc5ec4dc35ef29023eec9a3a68e65:functions/api/routes/user/keywordCreatePOST.js
     
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
