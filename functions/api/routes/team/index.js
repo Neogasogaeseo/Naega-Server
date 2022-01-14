18bbd00b2ce63916af9115345aff01358a820a4f
realwhyjay <brandon94_@naver.com>
2022-01-15 04:23:55 +0900

    feat:> 피드백 생성 작업 조금 진행 #19

--- functions/api/routes/team/index.js
+++ functions/api/routes/team/index.js
@@ -4,10 +4,11 @@ const { checkUser } = require('../../../middlewares/auth');
 
 router.post('/', checkUser, require('./teamPOST'));
 
-
 router.get('/', checkUser, require('./teamGET'));
 router.get('/issue', checkUser, require('./teamIssueGET'));
 router.get('/detail', checkUser, require('./teamDetailGET'));
 router.get('/detail/issue', checkUser, require('./teamDetailIssueGET'));
+
+router.get('/member', checkUser, require('./teamMemberListGET'));
 
 module.exports = router;
