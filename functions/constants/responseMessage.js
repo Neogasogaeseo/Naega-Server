d1bcc8f204a9eac09c0502a64bf764f8a14a217a
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-01-15 00:08:09 +0900

    chore:> conflict 해결

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -4,9 +4,6 @@ module.exports = {
 
   // 회원가입
   CREATED_USER: '회원 가입 성공',
-  DELETE_USER: '회원 탈퇴 성공',
-  ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
-  LENGTH_LIMIT_FAIL: '글자수는 1~6자 사이여야 합니다.',
   NEED_REGISTER: '회원 가입 필요',
 
   // 로그인
@@ -21,17 +18,9 @@ module.exports = {
   READ_PROFILE_SUCCESS: '프로필 조회 성공',
   READ_USER_SUCCESS: '유저 조회 성공',
 
-  // poisting
-  READ_ALL_POSTS: '모든 게시글 조회 성공',
-  READ_POST: '게시글 조회 성공',
-  POST_POST: '게시글 생성 성공',
-  NO_POST: '존재하지 않는 게시글입니다.',
-  UPDATE_POST: '게시글 수정 성공',
-  DELETE_POST: '게시글 삭제 성공',
-
   // keyword
-  ALREADY_KEYWORD : '이미 존재하는 키워드입니다',
-  ADD_KEYWORD_SUCCESS : '키워드 생성 성공',
+  ALREADY_KEYWORD: '이미 존재하는 키워드입니다',
+  ADD_KEYWORD_SUCCESS: '키워드 생성 성공',
 
   // 팀 정보
   READ_TEAM: '팀 정보 조회 성공',
@@ -43,15 +32,15 @@ module.exports = {
 
   // 팀원 정보
   READ_MEMBER: '팀원 정보 조회 성공',
-  READ_ALL_TEAM: '모든 팀원 정보 조회 성공',
+  READ_ALL_TEAM_MEMBER: '모든 팀원 정보 조회 성공',
   POST_MEMBER: '팀원 등록 성공',
   NO_MEMBER: '존재하지 않는 팀원입니다',
-
-  INTERNAL_SERVER_ERROR: '서버 내부 에러',
 
   // 팀원소개서
   READ_ALL_TEAM_SUCCESS: '팀 목록 조회 성공',
   READ_MY_ISSUE_SUCCESS: '나와 관련된 이슈 조회 성공',
   READ_TEAM_SUCCESS: '팀 조회 성공',
   READ_TEAM_ISSUE_SUCCESS: '팀 이슈 조회 성공',
+
+  INTERNAL_SERVER_ERROR: '서버 내부 에러',
 };
