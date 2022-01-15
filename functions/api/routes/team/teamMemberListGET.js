c05e118369b62e24a4191f216b3ce35dd2432842
realwhyjay <brandon94_@naver.com>
2022-01-15 14:07:39 +0900

    feat:> slack webHook 작업 #22

--- functions/api/routes/team/teamMemberListGET.js
+++ functions/api/routes/team/teamMemberListGET.js
@@ -4,10 +4,12 @@ const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const { memberDB } = require('../../../db');
+const slackAPI = require('../../../middlewares/slackAPI');
 
 module.exports = async (req, res) => {
   const user = req.user;
   const { teamId } = req.params;
+
   console.log('req.param :', req.params);
   if (!user) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
 
@@ -17,12 +19,14 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
 
     const getTeamMember = await memberDB.getAllTeamMemberByTeamId(client, teamId);
-    console.log('getTeamMember :', getTeamMember);
     res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_TEAM_MEMBER, getTeamMember));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
 
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.id}` : 'req.user 없음'} 
+    ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
