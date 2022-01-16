70022ebec3f4b9e14207cade5271cfbd1e2c86bd
Youjung Heo <hujumee27@gmail.com>
2022-01-17 02:40:15 +0900

    chore:> 파일명 변경

--- functions/api/routes/form/index.js
+++ functions/api/routes/form/index.js
@@ -5,7 +5,7 @@ const { checkUser } = require('../../../middlewares/auth');
 router.get('/template/popular', checkUser, require('./formTemplatePopularGET'));
 router.get('/template/recent', checkUser, require('./formTemplateRecentGET'));
 router.get('/create', checkUser, require('./formCreateGET'));
-router.get('/answer',require('./formAnswerRelationshipGET'));
+router.get('/answer',require('./formAnswerGET'));
 
 router.post('/answer', require('./formAnswerCreatePOST'));
 
