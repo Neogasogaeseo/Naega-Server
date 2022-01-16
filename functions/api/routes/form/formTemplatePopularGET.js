a01b77c86b9ca7f0f86e42b2d3a212011ff0f5a7
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-01-16 21:05:57 +0900

    feat:> 유저가 생성한 form인지 확인 #30

--- functions/api/routes/form/formTemplatePopularGET.js
+++ functions/api/routes/form/formTemplatePopularGET.js
@@ -6,13 +6,25 @@ const db = require('../../../db/db');
 const slackAPI = require('../../../middlewares/slackAPI');
 const { formDB } = require('../../../db');
 
+const extractValues = (arr, key) => {
+  if (!Array.isArray(arr)) return [arr[key] || null];
+  return [...new Set(arr.map((o) => o[key]).filter(Boolean))];
+};
+
 module.exports = async (req, res) => {
+  const { id: userId } = req.user;
   let client;
 
   try {
     client = await db.connect(req);
 
-    const data = await formDB.getAllFormPopular(client);
+    const form = await formDB.getAllFormPopular(client);
+    const formIdList = extractValues(form, 'id');
+    const isCreated = await formDB.getFormIsCreatedByUserId(client, formIdList, userId);
+    const map = new Map();
+    form.forEach((item) => map.set(item.id, item));
+    isCreated.forEach((item) => map.set(item.id, { ...map.get(item.id), ...item }));
+    const data = Array.from(map.values());
 
     res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_FORM_POPULAR_SUCCESS, data));
   } catch (error) {
