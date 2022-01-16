ae4389af61b83c20787f57cfd553bb2dfa67e3ec
realwhyjay <brandon94_@naver.com>
2022-01-16 23:46:12 +0900

    feat:> 팀 이슈 디테일 작업중 #34

--- functions/api/routes/team/index.js
+++ functions/api/routes/team/index.js
@@ -19,4 +19,6 @@ router.get('/issue/category', require('./teamIssueCategoryGET'));
 router.get('/member/:teamId', checkUser, require('./teamMemberListGET'));
 router.post('/feedback', checkUser, require('./teamFeedbackCreatePOST'));
 
+router.get('/', checkUser, require('./teamIssueDetailFeedbackGET'));
+
 module.exports = router;
