9a12d186571aa84270417bf29cfb02bc4ac04898
realwhyjay <brandon94_@naver.com>
2022-01-16 23:34:32 +0900

    fix:> memberDB 오류 수정

--- functions/db/memberDB.js
+++ functions/db/memberDB.js
@@ -63,13 +63,11 @@ const getAllTeamMemberByTeamId = async (client, teamId) => {
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
-
-
 //^_^// 팀에 멤버를 추가하는 쿼리
 const addMember = async (client, teamId, userIdList) => {
-  if(!userIdList) {
+  if (!userIdList) {
     return [];
-  };
+  }
 
   const valuesInsertQuery = userIdList.map((x) => `(${teamId}, ${x})`).join(', ');
   const { rows: resultRows } = await client.query(
@@ -84,10 +82,9 @@ const addMember = async (client, teamId, userIdList) => {
   return convertSnakeToCamel.keysToCamel(resultRows);
 };
 
-
 //^_^// 해당 멤버가 팀의 host인지 확인하는 과정
 const checkMemberHost = async (client, userId, teamId) => {
-  const { rows } = await client.query (
+  const { rows } = await client.query(
     `
     SELECT m.team_id, m.user_id    
     FROM "member" m
@@ -103,5 +100,4 @@ const checkMemberHost = async (client, userId, teamId) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
-
-module.exports = { getAllTeamByUserId,addMemberToTeam, getAllTeamMemberByTeamId, addMember, checkMemberHost, updateMemberAccept, updateMemberReject};
+module.exports = { getAllTeamByUserId, getAllTeamMemberByTeamId, addMember, checkMemberHost, updateMemberAccept, updateMemberReject };
