0c974c544ce93b2811f4f7b8908c85c770ddcf3a
realwhyjay <brandon94_@naver.com>
2022-01-20 01:42:12 +0900

    feat:> team member 조회 조건 추가

--- functions/api/routes/team/teamMemberListGET.js
+++ functions/api/routes/team/teamMemberListGET.js
@@ -15,8 +15,13 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-    const getTeamMember = await memberDB.getAllTeamMemberByTeamId(client, teamId);
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_TEAM_MEMBER, getTeamMember));
+    let teamMember = await memberDB.getAllTeamMemberByTeamId(client, teamId);
+
+    teamMember = teamMember.filter(function (x) {
+      if (x.id != user.id) return x;
+    });
+
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_TEAM_MEMBER, teamMember));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
@@ -24,7 +29,7 @@ module.exports = async (req, res) => {
     const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.id}` : 'req.user 없음'} 
     ${JSON.stringify(error)}`;
     slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
   }
