d3f6ff360fbafce9e8df5f69e4c8daee906c05d7
realwhyjay <brandon94_@naver.com>
2022-01-20 03:42:27 +0900

    fix:> response Message 수정

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -69,7 +69,7 @@ module.exports = {
   READ_FORM_SUCCESS: '폼 정보 조회 성공',
   FORM_CREATE_FAIL: '이미 존재하는 폼입니다',
   ANSWER_CREATE_SUCCESS: '답변 등록 성공',
-  NO_FORM: '해당 유저와 폼 아이디로 생성된 폼이 없습니다.',
+  // NO_FORM: '해당 유저와 폼 아이디로 생성된 폼이 없습니다.',
   READ_FORM_BANNER_SUCCESS: '배너 조회 성공',
   READ_FORM_DETAIL_SUCCESS: '폼 디테일 조회 성공',
 };
