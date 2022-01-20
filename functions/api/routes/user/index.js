70fdbd9ca48371cb7502552c8bf56700ec60d23b
realwhyjay <brandon94_@naver.com>
2022-01-20 21:35:14 +0900

    feat:> 콩나물 국밥을 다먹고 서진이가 원하는 요청을 들어준 커밋

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
-router.post('/keyword',checkUser, require('./keywordCreatePOST'));
+router.post('/keyword', checkUser, require('./keywordCreatePOST'));
 router.get('/search', checkUser, require('./userSearchGET'));
-router.get('/keyword', checkUser, require('./keywordListGET'));
+router.get('/keyword', require('./keywordListGET'));
 router.get('/', checkUser, require('./userInformationByTokenGET'));
 /**오늘의 한마디..
  * router.get('/:profileId', require('./userInformationByProfileIdGET'));
  * router.get('/:profileId/answer', require());
  * router.get('/:profileId/team', require());
-*/
+ */
 
 module.exports = router;
