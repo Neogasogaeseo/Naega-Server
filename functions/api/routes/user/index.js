e49b5915e1267208a35e790fc197727b8cbfbabb
realwhyjay <brandon94_@naver.com>
2022-01-20 21:52:13 +0900

    feat:> 키워드 생성, 키워드 리스트 조회 checkUser 삭제

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
-router.post('/keyword', checkUser, require('./keywordCreatePOST'));
+router.post('/keyword', require('./keywordCreatePOST'));
 router.get('/search', checkUser, require('./userSearchGET'));
 router.get('/keyword', require('./keywordListGET'));
 router.get('/', checkUser, require('./userInformationByTokenGET'));
