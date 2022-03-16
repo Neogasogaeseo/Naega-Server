40df5b528efeb1f3b48af47f5278382d7dbc3252
Youjung Heo <hujumee27@gmail.com>
2022-03-16 17:56:24 +0900

    feat:> 아이디 중복체크 #123

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -3,12 +3,16 @@ const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
 router.post('/keyword', require('./keywordCreatePOST'));
+
+router.put('/edit', checkUser, require('./userProfileEditPUT'));
+
 router.get('/search', checkUser, require('./userSearchGET'));
 router.get('/keyword', require('./keywordListGET'));
 router.get('/', checkUser, require('./userInformationByTokenGET'));
 router.get('/:profileId', require('./userInformationByProfileIdGET'));
 router.get('/:profileId/answer', require('./userPinnedAnswerGET'));
 router.get('/:profileId/team', require('./userPinnedTeamGET'));
+router.get('/edit/:profileId', checkUser, require('./userIdCheckGET'));
 
 
 module.exports = router;
