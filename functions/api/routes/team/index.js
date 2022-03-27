882e58454fbca4612dd248b529bb87e4b6f082f6
Youjung Heo <hujumee27@gmail.com>
2022-03-28 00:24:49 +0900

    feat:> 팀 정보 수정을 위한 팀 정보 조회 #25

--- functions/api/routes/team/index.js
+++ functions/api/routes/team/index.js
@@ -6,6 +6,7 @@ const uploadImage = require('../../../middlewares/uploadImage');
 router.post('/', checkUser, uploadImage('team'), require('./teamPOST'));
 router.post('/issue', checkUser, uploadImage('issue'), require('./teamIssuePOST'));
 
+router.get('/edit/:teamId', checkUser, require('./teamEditGET'));
 router.put('/edit', checkUser, require('./teamEditPUT'));
 
 router.get('/', checkUser, require('./teamGET'));
