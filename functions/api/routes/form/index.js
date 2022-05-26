f43cfc4d8baf08869010b4693621e821768a1956
주어랑 <63996052+ozzing@users.noreply.github.com>
2022-05-26 15:26:10 +0900

    hotfix:> formMyNewGET 누락 수정

--- functions/api/routes/form/index.js
+++ functions/api/routes/form/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
 
 router.get('/', checkUser, require('./formMyGET'));
-router.get('/new', checkUser, require('./formMyNewGET'));
 router.get('/all', checkUser, require('./formMyAllGET'));
 router.get('/template/popular', checkUser, require('./formTemplatePopularGET'));
 router.get('/template/recent', checkUser, require('./formTemplateRecentGET'));
