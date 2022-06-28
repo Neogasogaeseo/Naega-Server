a1305f5327d2cc27045dc48ac5e37ca67acc0101
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-06-28 16:29:54 +0900

    feat:> 나간 팀 알림 목록에서 제외 #195

--- functions/api/routes/user/userNoticeListGET.js
+++ functions/api/routes/user/userNoticeListGET.js
@@ -17,7 +17,11 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-    const invitedTeamIdList = await memberDB.getInvitedTeamIdList(client, userId, offset, limit);
+    const tempTeamIdList = await memberDB.getInvitedTeamIdList(client, userId, offset, limit);
+    const invitedTeamIdList = tempTeamIdList.filter((o) => {
+      if (o.isDeleted === true && o.isConfirmed === true) return false;
+      return true;
+    });
     const teamIdList = invitedTeamIdList.map((o) => o.teamId);
     if (teamIdList.length === 0) {
       const notice = [];
